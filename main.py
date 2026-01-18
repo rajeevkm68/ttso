a99fa67fe605e7a4a381b246a129bb0f7d04d3e1
rajeev <rajeevkm68@gmail.com>
2026-01-18 12:28:05 +0530

    Final Commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template,request, redirect, url_for
+
+from boto3 import Session
+from botocore.exceptions import BotoCoreError, ClientError
+from contextlib import closing
+import os
+import sys
+import subprocess
+from tempfile import gettempdir
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():  # put application's code here
+    return render_template("index.html")
+
+
+@app.route('/upload', methods=['POST'])
+def file_upload():
+    file = request.files.get('file')
+
+    if not file or file.filename == "":
+        return "No file selected", 400
+
+    # save file (example)
+    file.save(f"uploads/{file.filename}")
+
+    TTS()
+
+    return redirect(url_for('index'))
+
+def TTS():
+
+        polly = Session(profile_name="rajeev_dev").client("polly")
+
+        response = polly.synthesize_speech(
+            Text="Hello world!",
+            OutputFormat="mp3",
+            VoiceId="Joanna"
+        )
+
+        with open("speech.mp3", "wb") as f:
+            f.write(response["AudioStream"].read())
+
+
+
+if __name__ == '__main__':
+    app.run()
